a597c1e949e11b199fac4c0b9278c653a8492e3e
doxcom <aldoperez1319@gmail.com>
2025-05-27 15:48:16 -0600

    new index file added, next step connect to api gateway

--- lambda/get/index.mjs
+++ lambda/get/index.mjs
@@ -1,19 +1,46 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand,ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const getCoffee = async (event) => {
-  const command = new GetCommand({
-    TableName: "CoffeeShop",
-    Key: {
-      conffeeId: "c123",
-    },
-  });
+const tableName = process.env.tableName || "CoffeeShop";
 
-  const response = await docClient.send(command);
-  console.log(response);
-  return response;
+const createResponse = (statusCode, body) => {
+    const responseBody = JSON.stringify(body);
+    return {
+        statusCode,
+        headers: { "Content-Type": "application/json" },
+        body: responseBody,
+    };
 };
+
+export const getCoffee = async (event) => {
+    const { pathParameters } = event;
+    const { id } = pathParameters || {};
+
+    try {
+        let command;
+        if (id) {
+            command = new GetCommand({
+                TableName: tableName,
+                Key: {
+                    "coffeeId": id,
+                },
+            });
+        }
+        else {
+            command = new ScanCommand({
+                TableName: tableName,
+            });
+        }
+        const response = await docClient.send(command);
+        return createResponse(200, response);
+    }
+    catch (err) {
+        console.error("Error fetching data from DynamoDB:", err);
+        return createResponse(500, { error: err.message });
+    }
+
+}
 
---
+++ lambda/get/index1.mjs
@@ -0,0 +1,19 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand,ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export const getCoffee = async (event) => {
+  const command = new GetCommand({
+    TableName: "CoffeeShop",
+    Key: {
+      conffeeId: "c123",
+    },
+  });
+
+  const response = await docClient.send(command);
+  console.log(response);
+  return response;
+};
+
